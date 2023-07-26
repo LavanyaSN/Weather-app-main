b45ceab28c0c5cdf54fe191fa9438747bb3fff5b
lavu <lavu@gmail.com>
2023-07-26 10:37:24 +0530

    initial commit

---
+++ weather-app-main/client/src/components/LocationContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Typography, Box, styled } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  borderRadius: "10px",
+}));
+
+const LocationContainer = ({ location, country }) => {
+  return (
+    <StyledBox marginBottom={2} className="location-container">
+      <Container>
+        <Typography align="left">
+          <Typography variant="h2">{location}</Typography>
+          <Typography variant="h3">{country}</Typography>
+        </Typography>
+      </Container>
+    </StyledBox>
+  );
+};
+
+export { StyledBox };
+
+export default LocationContainer;
---
+++ weather-app-main/client/src/components/WeatherContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, Typography, Container } from "@mui/material";
+
+export default function WeatherContainer({ weather }) {
+  return (
+    <Box
+      sx={{
+        justifyContent: "center",
+      }}
+    >
+      <Container>
+        <Typography variant="h3">{weather}</Typography>
+      </Container>
+    </Box>
+  );
+}
